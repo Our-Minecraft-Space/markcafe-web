7f85b429e64c94d68924cd8699b1bfc3583d307c
Final Child <finalchild2@gmail.com>
2020-01-08 15:20:43 +0900

    Initial commit

---
+++ src/index.mjs
@@ -0,0 +1,56 @@
+import MarkdownIt from 'markdown-it';
+import containerPlugin from 'markdown-it-container'
+import juice from 'juice/client';
+
+const template1Start = `<div class="container">`;
+const template1End = `</div>`
+
+main();
+
+async function main() {
+    const config = await (await fetch('https://markcafe.finalchild.me/markcafe-config.json')).text();
+    const md = new MarkdownIt(config.markdownItOptions).use(containerPlugin, 'tip', {
+        render: (tokens, idx) => {
+            if (tokens[idx].nesting === 1) {
+                return `<div class="tip-header">${config.tipHeaderContent}</div>\n<div class="tip">`;
+            } else {
+                return `</div>\n`;
+            }
+        }
+    });
+    const oldImageRule = md.renderer.rules.image;
+    md.renderer.rules.image = (tokens, idx, options, env, slf) => {
+        if (tokens[idx].nesting !== 0) return oldImageRule(tokens, idx, options, env, slf);
+        if (tokens[idx].attrGet('src').startsWith('images/')) {
+            tokens[idx].attrSet('src', config.imgSrcPrefix + tokens[idx].attrGet('src'));
+        }
+        let result = oldImageRule(tokens, idx, options, env, slf);
+        if (idx === 0) {
+            result = `<div class="img-wrapper">` + result;
+        }
+        if (idx === tokens.length - 1) {
+            result = result + `</div>`;
+        }
+        return result;
+    };
+    
+    css = await (await fetch(`https://markcafe.finalchild.me/default${document.getElementById('is-non-wide').checked ? '-non-wide' : ''}.css`)).text()
+    
+    document.getElementById('convert-button').addEventListener('click', async event => {
+        document.getElementById('out').value = renderMdToCafeHtml(md, css, document.getElementById('in').value);
+    });
+}
+
+function renderMdToCafeHtml(md, css, src) {
+    const rendered = md.render(src);
+    const templated1 = template1Start + rendered + template1End;
+    const juiced = juice.inlineContent(templated1, css, {
+        inlinePseudoElements: true,
+        preserveFontFaces: false,
+        preserveImpotant: false,
+        preserveMediaQueries: false,
+        preserveKeyFrames: false,
+        preservePseudos: false
+    });
+    return juiced;
+}
